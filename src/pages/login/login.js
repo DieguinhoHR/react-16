f7e4f8caa532f5a5e86749f2ce7cc9ce369f3090
Diego <diego_henriquerodrigues@hotmail.com>
2019-03-05 23:09:15 -0300

    Add styled components

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,41 @@
 import React from 'react'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+import styled from 'styled-components'
 
-const Login = () => <h1>Login</h1>
+const Login = () => (
+  <Container>
+    <Grid container justify='center' spacing={40}>
+      <Grid item>
+        <Logo />
+      </Grid>
+
+      <Grid item xs={12}>
+        <GitHubButton>
+          Entrar com Github
+        </GitHubButton>
+      </Grid>
+    </Grid>
+  </Container>
+)
+
+const Container = styled.div`
+  padding: 20px;
+`
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 20px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
 
 export default Login
